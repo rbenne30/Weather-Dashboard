8fcab009ab3235f086e4022b87f5cd29958029fd
rbenne30 <ryannebennett1@outlook.com>
2022-03-11 15:13:50 -0500

    updated homepage.js

---
+++ assets /js/homepage.js
@@ -0,0 +1,20 @@
+ var url = "https://api.openweathermap.org/data/2.5/weather?q=New york&appid=750fd3564acf9a5eea60e2988ce03a06&units=imperial"
+ fetch(url).then(function(response){
+console.log(response);
+return response.json()
+ })
+ .then(function(data){
+console.log(data);
+ })
+ var searchBtn = document.getElementById("startbtn")
+ console.log(searchBtn)
+ searchBtn.addEventListener("click", searchBtn);
+    // //fetch().then(function(response) {
+    //     response.json(). then(function(data) 
+    //     console.log(response);
+    //     return response.json();
+    // })
+    // .then(function(data) {
+    //     //do something with the json data
+    //     console.log(data);
+    // });
